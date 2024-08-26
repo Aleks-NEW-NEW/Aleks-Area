ef052c1fb4a83057daec92564861debba3f79a65
Aleks-NEW-NEW <aleksandr101199@mail.ru>
2024-08-26 22:58:28 +0300

    Add files via upload

---
+++ Lessone_1.py
@@ -0,0 +1,29 @@
+#1st program
+print(9**0.5 * 5)
+
+print()
+
+#2nd program
+print(9.99 >= 9.98 and 1000 != 1000.1)
+
+print()
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+print()
+
+#4th program
+print(int(float('123.456') * 10) % 10)
+
+print()
+
+#4th program, version_2
+print(int(float('123.456') // 30))
+
+print()
+
+#4th program, version_3
+print(bool('123.456') and '4')
