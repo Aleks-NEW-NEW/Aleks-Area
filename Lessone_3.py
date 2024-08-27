cb087b1448778efdc85142809855980031c61e0b
Aleks-NEW-NEW <aleksandr101199@mail.ru>
2024-08-27 23:20:28 +0300

    Add files via upload

---
+++ Lessone_3.py
@@ -0,0 +1,6 @@
+amount_HW_completed = 12
+amount_hours_spent = 1.5
+name_course = 'Python'
+time_for_one_task = amount_hours_spent / amount_HW_completed
+
+print('Курс:', str(name_course) + ', всего задач:' + str(amount_HW_completed) + ', затрачено часов:', str(amount_hours_spent) + ', среднее время выполнения', time_for_one_task, 'часа.')
