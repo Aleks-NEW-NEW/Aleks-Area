a3608611059879e5e29ecc9f9590c06d5073c45a
Aleks-NEW-NEW <aleksandr101199@mail.ru>
2024-10-06 17:59:57 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,103 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if (new_floor > self.number_of_floors) or (new_floor < 1):
+            return print('"Такого этажа не существует"')
+        for i in range(1, new_floor + 1):
+            print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        self.data_type_error_1()
+        return self
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        self.data_type_error_1()
+        return self
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        self.data_type_error_1()
+        return self
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        self.data_type_error_1()
+        return self
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        self.data_type_error_1()
+        return self
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        self.data_type_error_1()
+        return self
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors = self.number_of_floors + other
+            return self
+        self.data_type_error_1()
+        return self
+
+    def __radd__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors = self.number_of_floors + other
+            return self
+        self.data_type_error_1()
+        return self
+
+    def __iadd__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        self.data_type_error_1()
+        return self
+
+    def data_type_error_1(self):
+        self.number_of_floors = 13
+        print('Ошибка: неверно задан тип данных для операции.\n'
+              'Кол-во этажей изменено на 13.')
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
